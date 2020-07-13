76e8235bac975de0e729a5c0a06a0429fff37b6b
Matheus <raydra10@gmail.com>
2020-07-13 17:25:59 -0300

    Commit on Git

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+// iniciando app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// iniciando o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{ userNewUrlParser: true}
+);
+requireDir('./src/models');
+
+
+app.use('/api', require('./src/routes'));
+
+
+app.listen(3001);
